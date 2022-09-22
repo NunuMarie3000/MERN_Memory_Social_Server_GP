86114e83bed79d974061ee117ff73199aa120b81
Storm O'Bryant <vmarie1997@gmail.com>
2022-09-21 20:41:16 -0500

    hotfix on comments route

--- routes/commentsRoute.js
+++ routes/commentsRoute.js
@@ -1,14 +1,19 @@
 // get all comments associated with a memory
 const express = require('express')
 const router = express.Router()
-const memoryModel = require('../models/memoryModel')
+// const memoryModel = require('../models/memoryModel')
+const commentModel = require('../models/commentModel')
 
 router.get('/comments/:memoryid', async (req,res)=>{
     // client needs to send author's id in the body
     const memoryId = req.params.memoryid
     try {
-      const memory = await memoryModel.findById(memoryId).populate("comments")
-      res.status(200).send(memory.comments)
+      // const memory = await memoryModel.findById(memoryId).populate("comments")
+
+      // what if instead of this i did
+      const comments = await commentModel.findMany({"commented_on": memoryId})
+
+      res.status(200).send(comments)
     } catch (error) {
       res.send(error)
     }
